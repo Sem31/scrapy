09bf6e64541c6f1436e485779e3e62f5f40bab30
kamlesh <31kamleshprajapat@gmail.com>
2019-03-23 12:02:06 +0530

    use the beautiful soup for the seeing html code

--- scrapy.py
+++ scrapy.py
@@ -1,6 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = 'https://docs.python.org/3/'
 response = requests.get(url)
 html = response.content
-print (html)
+
+soup = BeautifulSoup(html)
+print (soup.prettify())
