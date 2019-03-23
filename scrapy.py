a885274ee7f3a0a99472a852d4e43726b2e10322
kamlesh <31kamleshprajapat@gmail.com>
2019-03-23 12:04:10 +0530

    now change is that we fetch all tbody from the given website using beautifulsoup

--- scrapy.py
+++ scrapy.py
@@ -5,5 +5,6 @@ url = 'https://docs.python.org/3/'
 response = requests.get(url)
 html = response.content
 
-soup = BeautifulSoup(html)
-print (soup.prettify())
+soup = BeautifulSoup(html,'lxml')
+table = soup.find('tbody', attrs={'class': 'stripe'})
+print(soup.table)
